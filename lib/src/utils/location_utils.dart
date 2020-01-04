c41aa639bdcedd50051bf823368827ed2073a583
János Roden <janos.roden@blacksalt.io>
2020-01-04 18:51:55 +0100

    cache app headers

--- lib/src/utils/location_utils.dart
+++ lib/src/utils/location_utils.dart
@@ -6,8 +6,6 @@ import 'package:flutter/services.dart';
 import '../model/location_result.dart';
 
 class LocationUtils {
-  static const platform = const MethodChannel('google_map_location_picker');
-
   static String getAddressComponent(String type, List addressComponents) {
     for (Map<String, dynamic> component in addressComponents) {
       if ((component['types'] as List).contains(type))
@@ -38,29 +36,34 @@ class LocationUtils {
     locationResult.latLng = latLng;
   }
 
+  static const _platform = const MethodChannel('google_map_location_picker');
+  static Map<String, String> _appHeaderCache = {};
   static Future<Map<String, String>> getAppHeaders() async {
-    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    if (_appHeaderCache.isEmpty)
+    {
+      PackageInfo packageInfo = await PackageInfo.fromPlatform();
 
-    if (Platform.isIOS)
-      return {
-        "X-Ios-Bundle-Identifier": packageInfo.packageName,
-      };
-
-    if (Platform.isAndroid) {
-      String sha1;
-      try {
-        sha1 = await platform.invokeMethod(
-            'getSigningCertSha1', packageInfo.packageName);
-      } on PlatformException {
-        return {};
+      if (Platform.isIOS) {
+          _appHeaderCache = {
+            "X-Ios-Bundle-Identifier": packageInfo.packageName,
+          };
       }
+      else if (Platform.isAndroid) {
+        String sha1;
+        try {
+          sha1 = await _platform.invokeMethod(
+              'getSigningCertSha1', packageInfo.packageName);
+        } on PlatformException {
+          _appHeaderCache = {};
+        }
 
-      return {
-        "X-Android-Package": packageInfo.packageName,
-        "X-Android-Cert": sha1,
-      };
+        _appHeaderCache = {
+          "X-Android-Package": packageInfo.packageName,
+          "X-Android-Cert": sha1,
+        };
+      }
     }
-
-    return {};
+      
+    return _appHeaderCache;
   }
 }
